b3990f89a6b3659ee1c12cfa2df340711c1f8211
anish-garg <anishgarg5914@gmail.com>
2024-04-10 02:09:20 +0530

    navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import trees from "../assets/matt-artz-nTRDnDdDYk8-unsplash.jpg"
+
+const Hero = () => {
+    return (
+        <img src={trees} alt="trees"/>
+    )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div className="flex text-lg justify-center items-center gap-20 bg-white w-auto h-16 z-0">
+      <div className="flex flex-col justify-center items-center bg-green-600 text-white h-24 w-20 z-10 text-xl font-bold">
+      <div className="text-green-900" >Forest</div>
+      <div >Sight</div>
+      </div>
+      <div>Map</div>
+      <div>Dashboard</div>
+      <div>Newsletter</div>
+      <div>About</div>
+    </div>
+  )
+}
+
+export default Navbar
